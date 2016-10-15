34a9e2546fca63c69c15069096d2759750dec479
tomaka <htomaka@treeptik.fr>
2016-10-15 13:19:16 +0200

    added more exemples

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+//const + arrow function
 const helloBabel = () => {
   console.log('Hello Babel!');
 }
 
 helloBabel();
+
+// block scope
+if(true){
+  let foo = "bar";
+}
+
+console.log(foo); // foo is not defined error
